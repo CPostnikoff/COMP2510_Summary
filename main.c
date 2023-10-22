914134113858b1a58878fa0b11c1fba79ffa8546
Cameron Postnikoff <CameronJP@protonmail.com>
2023-10-21 17:35:46 -0700

    Refactor directory structure

--- main.c
+++ main.c
@@ -3,23 +3,5 @@
 
 int main() {
 
-    char firstName[20];
-    char lastName[20];
-
-    printf("please enter your first name: ");
-
-    char *myName = "cameroncameroncameroncameroncameron";
-
-    scanf("%s", firstName);
-    printf("firstName %s\n", firstName);
-
-    for (int i = 0; myName[i] != '\0'; i++) {
-        firstName[i] = myName[i];
-    }
-
-    for (int i = 0; firstName[i] != '\0'; i++) {
-        printf("%c", firstName[i]);
-    }
-
     return 0;
 }
